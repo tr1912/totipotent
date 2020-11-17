93388e671da3513efae4b959f1e25d11288ce4a2
wangxiao <wangxiao@ybm100.com>
2020-11-17 16:22:28 +0800

    整合xxl-jon

---
+++ toti-job/src/main/java/com/wx/lab/component/job/XxlJobApplication.java
@@ -0,0 +1,16 @@
+package com.wx.lab.component.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangxiao@ybm100.com
+ * @date 2020-11-17 上午12:07
+ * @projectname totipotent
+ */
+@SpringBootApplication
+public class XxlJobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobApplication.class, args);
+    }
+}
